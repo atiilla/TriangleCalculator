3eca5cf51bdc1fd2f98d18dc9e4f73ebe00895b1
intec <intec@intec.intecbrussel.be>
2018-06-12 09:07:51 +0200

    Hypotenuse method added

--- src/geometry/App.java
+++ src/geometry/App.java
@@ -6,8 +6,7 @@ public class App {
 	public static void main(String[] args) {
 
 		TriangleUtils triangle = new TriangleUtils();
-		
-		
+
 		Scanner in = new Scanner(System.in);
 		System.out.print("Side-1: ");
 		double side1 = in.nextDouble();
@@ -16,19 +15,19 @@ public class App {
 		System.out.print("Side-3: ");
 		double side3 = in.nextDouble();
 		System.out.println(
-				is_Valid(side1, side2, side3) ? "The area of the triangle is " + triangle.area_triangle(side1, side2, side3)
+				is_Valid(side1, side2, side3)
+						? "The area of the triangle is " + triangle.area_triangle(side1, side2, side3) + "\nHypotenuse: "
+								+ triangle.getHypotenuse()
 						: "Invalid triangle");
 	}
 
 	private static boolean is_Valid(double side1, double side2, double side3) {
-		
-			if (side1 + side2 > side3 && side2 + side3 > side1 && side1 + side3 > side2)
-				return true;
-			else
-				return false;
-		
-	}
 
-	
+		if (side1 + side2 > side3 && side2 + side3 > side1 && side1 + side3 > side2)
+			return true;
+		else
+			return false;
+
+	}
 
 }
--- src/geometry/TriangleUtils.java
+++ src/geometry/TriangleUtils.java
@@ -4,6 +4,7 @@ public class TriangleUtils {
 	private double side1;
 	private double side2;
 	private double side3;
+	private double hypotenuse;
 
 	public TriangleUtils() {
 
@@ -39,8 +40,6 @@ public class TriangleUtils {
 		this.side3 = side3;
 	}
 
-
-
 	public double area_triangle(double side1, double side2, double side3) {
 		this.side1 = side1;
 		this.side2 = side2;
@@ -50,4 +49,11 @@ public class TriangleUtils {
 		area = Math.sqrt(s * (s - side1) * (s - side2) * (s - side3));
 		return area;
 	}
+
+	public double getHypotenuse() {
+		this.side1 = side1;
+		this.side2 = side2;
+		return Math.sqrt(Math.pow(side1, 2) + Math.pow(side2, 2));
+	}
+
 }
